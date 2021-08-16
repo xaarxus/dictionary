e6bad324208d68bf1146abb93ab2a19f75d76cae
newsalaar <keyshet@gmail.com>
2021-08-16 15:52:30 +0300

    connect to Redux DevTools

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,11 @@ import './styles/light/index.sass';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers/index';
 import App from './components/App';
 
-const store = createStore(reducers);
+const store = createStore(reducers, composeWithDevTools());
 
 render(
     <Provider store={store}>
